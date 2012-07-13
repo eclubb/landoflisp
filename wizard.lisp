2dda1631472788a9d81ce6bcd8083586bfdc4703
Earle Clubb <eclubb@valcom.com>
2012-07-13 17:25:21 -0400

    Initial commit of wizard game.

---
+++ wizard.lisp
@@ -0,0 +1,19 @@
+(defvar *nodes* '((living-room (You are in the living room.
+                                A wizard is snoring loudly on the couch.))
+                  (garden (You are in a beautiful garden.
+                           There is a well in front of you.))
+                  (attic (You are in the attic.
+                          There is a giant welding torch in the corner.))))
+
+(defvar *edges* '((living-room (garden west door) (attic upstairs ladder))
+                  (garden (living-room east door))
+                  (attic (living-room downstairs ladder))))
+
+(defun describe-location (location nodes)
+  (cadr (assoc location nodes)))
+
+(defun describe-path (edge)
+  `(There is a ,(caddr edge) going ,(cadr edge) from here.))
+
+(defun describe-paths (location edges)
+  (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
