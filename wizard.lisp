cba2d975da451d9ca55f024c5d2886d8bc5bd430
Earle Clubb <earle@handfulofclubbs.net>
2012-07-13 23:14:29 -0400

    Add ability to walk, pick up objects, etc.

--- wizard.lisp
+++ wizard.lisp
@@ -9,6 +9,14 @@
                   (garden (living-room east door))
                   (attic (living-room downstairs ladder))))
 
+(defvar *objects* '(whiskey bucket frog chain))
+(defvar *object-locations* '((whiskey living-room)
+                             (bucket living-room)
+                             (frog garden)
+                             (chain garden)))
+
+(defvar *location* 'living-room)
+
 (defun describe-location (location nodes)
   (cadr (assoc location nodes)))
 
@@ -17,3 +25,36 @@
 
 (defun describe-paths (location edges)
   (apply #'append (mapcar #'describe-path (cdr (assoc location edges)))))
+
+(defun objects-at (loc objs obj-locs)
+  (labels ((at-loc-p (obj)
+              (eq (cadr (assoc obj obj-locs)) loc)))
+    (remove-if-not #'at-loc-p objs)))
+
+(defun describe-objects (loc objs obj-locs)
+  (labels ((describe-obj (obj)
+             `(You see a ,obj on the floor.)))
+    (apply #'append (mapcar #'describe-obj (objects-at loc objs obj-locs)))))
+
+(defun look ()
+  (append (describe-location *location* *nodes*)
+          (describe-paths *location* *edges*)
+          (describe-objects *location* *objects* *object-locations*)))
+
+(defun walk (direction)
+  (let ((next (find direction (cdr (assoc *location* *edges*)) :key #'cadr)))
+        (if next
+          (progn (setf *location* (car next))
+                 (look))
+          '(You cannot go that way.))))
+
+(defun pickup (object)
+  (cond ((member object (objects-at *location* *objects* *object-locations*))
+          (push (list object 'body) *object-locations*)
+          `(You are now carrying the ,object))
+         (t '(You cannot get that.))))
+
+(defun inventory ()
+    (append '(items -) (objects-at 'body *objects* *object-locations*)))
+
+(princ (look))
